a9f6e9d5a39b63bd53aad96f777fd48d1b7e2431
pavan1419 <ptechhat@gmail.com>
2024-10-21 19:38:42 +0530

    done last

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const path = require('path');
 const connectToDb = require('./DBConnet');
 const urlRoute = require('./routes/urlRouter');
 const URL = require('./models/urlModel');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'));
+
 app.use(express.json());
+
+app.get('/test', async (req, res) => {
+  const allUrls = await URL.find({});
+  return res.render('home', { urls: allUrls });
+});
+
+
 app.use('/url', urlRoute);
 
 connectToDb('mongodb://127.0.0.1:27017/sort-url').then(() => {
